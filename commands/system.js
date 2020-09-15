d4a56fe6b10ff37b0a657e123e588d1c8b503348
swdevlin <swdevlin@gmail.com>
2020-09-15 09:37:55 -0400

    uriEncode parameters
    move data to class member to make things easier to test
    
    Handle non-world returns from the name lookup

--- commands/system.js
+++ commands/system.js
@@ -19,6 +19,8 @@ class System extends BaseCommand {
   constructor(prefix, msg) {
     super(prefix, msg);
     this.systemName = null;
+    this.system = null;
+    this.world = null;
   }
 
   parseMsg() {
@@ -32,7 +34,7 @@ class System extends BaseCommand {
   }
 
   async findSystemByName(name) {
-      const travellerMapSearchURL = `https://travellermap.com/api/search?q=${this.systemName}`;
+      const travellerMapSearchURL = `https://travellermap.com/api/search?q=${encodeURIComponent(this.systemName)}`;
       const response = await axios.get(travellerMapSearchURL);
       if (response.status !== 200) {
         throw 'error fetching data';
@@ -82,52 +84,58 @@ class System extends BaseCommand {
   async renderMultipleMatches(systems) {
     let response = 'Multiple matches\n';
     for (const system of systems) {
-      const hex = this.makeHexCode(system.World.HexX, system.World.HexY);
-      response += `${system.World.Name}, ${system.World.Sector} ${hex}\n`;
+      if (system.hasOwnProperty('World')) {
+        const hex = this.makeHexCode(system.World.HexX, system.World.HexY);
+        response += `${system.World.Name}, ${system.World.Sector} ${hex}\n`;
+      }
     }
     await this.msg.reply(response);
   }
 
-  async renderSystemData(system) {
-    const sector = system.World.Sector;
-    const hex = this.makeHexCode(system.World.HexX, system.World.HexY);
+  async fetchWorldData() {
+    const sector = encodeURIComponent(this.system.World.Sector);
+    const hex = encodeURIComponent(this.makeHexCode(this.system.World.HexX, this.system.World.HexY));
     const travellerMapWorldURL = `https://travellermap.com/data/${sector}/${hex}`;
     const worldResponse = await axios.get(travellerMapWorldURL);
     if (worldResponse.status !== 200) {
       throw 'error fetching world data';
     }
-    const world = worldResponse.data.Worlds[0];
+    this.world = worldResponse.data.Worlds[0];
+  }
 
-    let response = `The ${world.Name} system is located in ${world.Sector} ${hex}\n`;
-    response += `It has a UWP of ${world.UWP}\n`;
-    response += `There are ${world.Worlds} planets orbiting the ${world.Stellar} star\n`;
+  async renderSystemData() {
+    if (this.world === null)
+      await this.fetchWorldData();
+    let response = `The ${this.world.Name} system is located in ${this.world.Sector} ${this.world.Hex}\n`;
+    response += `It has a UWP of ${this.world.UWP}\n`;
+    response += `There are ${this.world.Worlds} planets orbiting the ${this.world.Stellar} star\n`;
 
-    if (world.Zone === "A") {
+    if (this.world.Zone === "A") {
       response += "This world is an **Amber** zone\n";
-    } else if (world.Zone === "R") {
+    } else if (this.world.Zone === "R") {
       response += `This world is a **RED** zone\n`;
     } else {
       response += "There are no travel restrictions\n";
     }
 
-    if (world.PBG.slice(world.PBG.length - 1) === "0") {
+    if (this.world.PBG.slice(this.world.PBG.length - 1) === "0") {
       response += "There is **no** Gas Giant suitable for refueling in this system\n";
     } else {
       response += "There is at least one **Gas Giant** suitable for refueling in this system\n";
     }
 
-    response += `This world owes allegiance to the ${world.AllegianceName}\n`;
-    if (world.Bases.length > 0) {
+    response += `This world owes allegiance to the ${this.world.AllegianceName}\n`;
+    if (this.world.Bases.length > 0) {
       response += `It has the following bases:\n`;
-      for (const base of world.Bases) {
+      for (const base of this.world.Bases) {
         response += `\t${baseCodes[base].type}\n`;
       }
     } else {
       response += `There are no bases\n`;
     }
 
-    //parse remarks - this field is overloaded with items and needs to be unpacked. No I'm not bitter, why do you ask?
-    const tradeCodes = this.parseRemarks(world.Remarks);
+    // parse remarks - this field is overloaded with items and needs to be unpacked. No I'm not bitter, why do you ask?
+    const tradeCodes = this.parseRemarks(this.world.Remarks);
     response += `It has the trade codes of **${tradeCodes}**\n`;
 
     await this.msg.reply(response);
@@ -137,12 +145,14 @@ class System extends BaseCommand {
     await super.do();
     try {
       const systems = await this.findSystemByName();
-      if (systems.length === 1)
-        await this.renderSystemData(systems[0]);
-      else {
+      if (systems.length === 1) {
+        this.system = systems[0];
+        await this.renderSystemData();
+      } else {
         for (const system of systems) {
-          if (this.worldNamesMatch(this.systemName, system.World.Name)) {
-            return await this.renderSystemData(system);
+          if (system.hasOwnProperty('World') && this.worldNamesMatch(this.systemName, system.World.Name)) {
+            this.system = system;
+            return await this.renderSystemData();
           }
         }
         await this.renderMultipleMatches(systems);
