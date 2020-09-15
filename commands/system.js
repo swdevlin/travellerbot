71109031dee9b6c925b62bc06b8ceb8530c2face
swdevlin <swdevlin@gmail.com>
2020-09-14 23:19:19 -0400

    Merge in changes from Eric

--- commands/system.js
+++ commands/system.js
@@ -44,85 +44,40 @@ class System extends BaseCommand {
         return response.data.Results.Items;
       }
   }
-    parseRemarks (stringRemarks) {
-        const remarksRegex = /(.*)[\(\[](.*)/g;
-        let rem = remarksRegex.exec(stringRemarks);
-        // first item is the string searched
-        rem.shift();
-        let tcList = [];
-        let sopList = [];
-        let sophString = "";
-        if (rem) {
-            tcList = rem[0].split(" ");
-            sophString = rem[1];
-        }
-        // remove last item in tc list - it is empty
-        tcList.pop();
-        //discard the list of sophonts for now
-        //TODO parse sophonts into population splits
-        return tcList;
+
+  parseRemarks (stringRemarks) {
+    const remarksRegex = /(.*)[\(\[](.*)/g;
+    let tcList = [];
+    let rem = remarksRegex.exec(stringRemarks);
+    if (rem) {
+      // first item is the string searched
+      rem.shift();
+      let sopList = [];
+      let sophString = "";
+      if (rem) {
+        tcList = rem[0].split(" ");
+        sophString = rem[1];
+      }
+      // remove last item in tc list - it is empty
+      tcList.pop();
+      //discard the list of sophonts for now
+      //TODO parse sophonts into population splits
     }
+    return tcList;
+  }
 
-    getSystemInformation(name) {
-        let response = "";
-        const urlTravellerMap = "https://travellermap.com/";
-        const urlTravellerMapSearch = urlTravellerMap + `api/search?q=${name}`;
+  worldNamesMatch(name1, name2) {
+    return name1.localeCompare(name2, undefined, { sensitivity: 'base' }) === 0;
+  }
 
-        // Call the travellermap api using name in the search api
-
-        // extract the json response
-        const systemDetails = { "Results": { "Count": 1, "Items": [{ "World": { "HexX": 27, "HexY": 4, "Sector": "Deneb", "Uwp": "C543976-A", "SectorX": -3, "SectorY": -1, "Name": "Talon", "SectorTags": "Official OTU" } }] } };
-        const worldDetails = systemDetails.Results.Items[0].World;
-
-        // make the hexcdoe to use inthe data call
-        const hexCode = this.makeHexCode(worldDetails.HexX, worldDetails.HexY)
-        // call the data api using the hexcode
-        const urlTravellerMapData = urlTravellerMap + `data/${worldDetails.Sector}/${hexCode}`
-
-        const moreDetails = { "Worlds": [{ "Name": "Talon", "Hex": "2704", "UWP": "C543976-A", "PBG": "224", "Zone": "", "Bases": "CK", "Allegiance": "VAug", "Stellar": "K4 V", "SS": "D", "Ix": "{ 4 }", "CalculatedImportance": 4, "Ex": "(H8E+3)", "Cx": "[8D49]", "Nobility": "", "Worlds": 18, "ResourceUnits": 5712, "Subsector": 3, "Quadrant": 1, "WorldX": -70, "WorldY": -76, "Remarks": "Hi In Po (Souggvuez)8 Varg2", "LegacyBaseCode": "H", "Sector": "Deneb", "SubsectorName": "Million", "SectorAbbreviation": "Dene", "AllegianceName": "United Followers of Augurgh" }] };
-        const world = moreDetails.Worlds[0];
-
-        // do something with system details
-        response += `The search url was <${urlTravellerMapSearch}>\n`;
-        response += `The data url was <${urlTravellerMapData}>\n`
-
-        response += `The ${world.Name} system is in the ${worldDetails.Sector} sector\n`;
-        response += `It has a UWP of ${worldDetails.Uwp}\n`;
-        response += `The Hex Code for this system is ${hexCode}\n`;
-
-        if (world.Zone == "A") {
-            response += "This world is an **Amber** zone\n";
-        } else {
-            if (world.Zone == "R") {
-                response += `This world is a **RED** zone\n`;
-            } else {
-                response += "This world in **not** an Amber or Red Zone world\n";
-            }
-        }
-        if (world.PBG.slice(world.PBG.length - 1) == "0") {
-            response += "There is **no** Gas Giant suitable for refueling in this system\n";
-        } else {
-            response += "There is at least one **Gas Giant** suitable for refueling in this system\n";
-        }
-        response += `This world owes allegiance to the ${world.AllegianceName}\n`;
-        response += `It has the following bases:\n`;
-        for (const b of world.Bases) {
-            response += `\t${baseCodes[b].type}\n`;
-        }
-        //parse remarks - this field is overloaded with items and needs to be unpacked. No I'm not bitter, why do you ask?
-        const tradeCodes = this.parseRemarks(world.Remarks);
-        response += `It has the trade codes of **${tradeCodes}**\n`;
-        return response;
-    }
-
-    toTitleCase(str) {
-        return str.replace(
-            /\w\S*/g,
-            function (txt) {
-                return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-            }
-        );
-    }
+  toTitleCase(str) {
+      return str.replace(
+          /\w\S*/g,
+          function (txt) {
+              return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+          }
+      );
+  }
 
   async renderMultipleMatches(systems) {
     let response = 'Multiple matches\n';
@@ -142,12 +97,39 @@ class System extends BaseCommand {
       throw 'error fetching world data';
     }
     const world = worldResponse.data.Worlds[0];
-    let response = `${world.Name}\n`;
-    response += `${world.UWP}\n`;
-    response += `${world.Stellar}\n`;
-    response += `${world.Remarks}\n`;
-    response += `${world.Bases}\n`;
-    response += `${world.AllegianceName}\n`;
+
+    let response = `The ${world.Name} system is located in ${world.Sector} ${hex}\n`;
+    response += `It has a UWP of ${world.UWP}\n`;
+    response += `There are ${world.Worlds} planets orbiting the ${world.Stellar} star\n`;
+
+    if (world.Zone === "A") {
+      response += "This world is an **Amber** zone\n";
+    } else if (world.Zone === "R") {
+      response += `This world is a **RED** zone\n`;
+    } else {
+      response += "There are no travel restrictions\n";
+    }
+
+    if (world.PBG.slice(world.PBG.length - 1) === "0") {
+      response += "There is **no** Gas Giant suitable for refueling in this system\n";
+    } else {
+      response += "There is at least one **Gas Giant** suitable for refueling in this system\n";
+    }
+
+    response += `This world owes allegiance to the ${world.AllegianceName}\n`;
+    if (Array.isArray(world.Bases)) {
+      response += `It has the following bases:\n`;
+      for (const base of world.Bases) {
+        response += `\t${baseCodes[base].type}\n`;
+      }
+    } else {
+      response += `There are no bases\n`;
+    }
+
+    //parse remarks - this field is overloaded with items and needs to be unpacked. No I'm not bitter, why do you ask?
+    const tradeCodes = this.parseRemarks(world.Remarks);
+    response += `It has the trade codes of **${tradeCodes}**\n`;
+
     await this.msg.reply(response);
   }
 
@@ -157,17 +139,18 @@ class System extends BaseCommand {
       const systems = await this.findSystemByName();
       if (systems.length === 1)
         await this.renderSystemData(systems[0]);
-      else
+      else {
+        for (const system of systems) {
+          if (this.worldNamesMatch(this.systemName, system.World.Name)) {
+            return await this.renderSystemData(system);
+          }
+        }
         await this.renderMultipleMatches(systems);
+      }
     } catch (error) {
+      console.log(error);
       this.msg.reply('Error looking up system');
     }
-    // let response = '\n';
-    // for (const name of tokens) {
-    //     response += `System Information: **${this.toTitleCase(name)}**\n`;
-    //     response += this.getSystemInformation(name);
-    // }
-    // await this.msg.reply(response);
   }
 
 }
