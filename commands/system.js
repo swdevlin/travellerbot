253b9fa9604fb556323487ad54553b2547da38ec
Eric Stevens <eric@ericstevens.ca>
2020-09-14 21:33:27 -0400

    improved system display

--- commands/system.js
+++ commands/system.js
@@ -1,120 +1,139 @@
 const BaseCommand = require("./base");
 
-const {tradeCodes, purchase, sell} = require("../trade_codes");
+const { tradeCodes, purchase, sell } = require("../trade_codes");
 
 const baseCodes = {
-    "A": {Allegience: "Imperial", Type: "Naval and Scout Bases"},
-    "B": {Allegience: "Imperial", Type: "Naval Base and Way Station"},
-    "C": {Allegience: "Vargr", Type: "Corsair Base"},
-    "D": {Allegience: "Imperial", Type: "Depot"},
-    "E": {Allegience: "Hiver", Type: "Embassy Center"},
-    "F": {Allegience: "", Type: "Military and Naval Base"},
-    "G": {Allegience: "Vargr", Type: "Naval Base"},
-    "H": {Allegience: "Vargr", Type: "Naval and Corsair Base"},
-    "I": {Allegience: "", Type: "Naval Base"},
-    "J": {Allegience: "Imperial", Type: "Naval and Scout"},
-    "K": {Allegience: "Imperial", Type: "Naval and Scout"},
-    "L": {Allegience: "Imperial", Type: "Naval and Scout"},
-    "M": {Allegience: "Imperial", Type: "Naval and Scout"},
-    "N": {Allegience: "Imperial", Type: "Naval and Scout"},
-    "O": {Allegience: "Imperial", Type: "Naval and Scout"},
-    "P": {Allegience: "Imperial", Type: "Naval and Scout"},
-    "Q": {Allegience: "Imperial", Type: "Naval and Scout"},
-    "R": {Allegience: "Imperial", Type: "Naval and Scout"},
-    "S": {Allegience: "Imperial", Type: "Naval and Scout"},
-    "T": {Allegience: "Imperial", Type: "Naval and Scout"},
-    "U": {Allegience: "Imperial", Type: "Naval and Scout"},
-    "V": {Allegience: "Imperial", Type: "Naval and Scout"},
-    "W": {Allegience: "Imperial", Type: "Naval and Scout"},
-    "X": {Allegience: "Imperial", Type: "Naval and Scout"},
-    "Y": {Allegience: "Imperial", Type: "Naval and Scout"},
-    "Z": {Allegience: "Imperial", Type: "Naval and Scout"}
+    "C": { allegiance: "Vargr", type: "Corsair Base" },
+    "D": { allegiance: "Any", type: "Naval Depot" },
+    "E": { allegiance: "Hiver", type: "Embassy" },
+    "K": { allegiance: "Any", type: "Naval Base" },
+    "M": { allegiance: "Any", type: "Military Base" },
+    "N": { allegiance: "Imperial", type: "Naval Base" },
+    "R": { allegiance: "Aslan", type: "Clan Base" },
+    "S": { allegiance: "Imperial", type: "Scout Base" },
+    "T": { allegiance: "Aslan", type: "Tlaukhu Base" },
+    "V": { allegiance: "Any", type: "Exploration Base" },
+    "W": { allegiance: "Any", type: "Way Station" }
 }
 
 class System extends BaseCommand {
-  constructor(prefix, msg) {
-    super(prefix, msg);
-    this.codes = [];
-    this.goodName = "";
-  }
-
-  parseMsg() {
-    let tokens = this.commandText.split(' ');
-    // get rid of the trade
-    tokens.shift();
-    this.codes = tokens.map(a => a.toUpperCase());
-  }
-
-  makeHexCode(hexX, hexY) {
-    let hexXOut = hexX.toString();
-    let hexYOut = hexY.toString();
-    if (hexXOut.length < 2) {
-        hexXOut = "0" + hexXOut;    
+    constructor(prefix, msg) {
+        super(prefix, msg);
+        this.codes = [];
+        this.goodName = "";
     }
-    if (hexYOut.length < 2) {
-        hexYOut = "0" + hexYOut;    
+
+    parseMsg() {
+        let tokens = this.commandText.split(' ');
+        // get rid of the trade
+        tokens.shift();
+        this.codes = tokens.map(a => a.toUpperCase());
     }
-    return hexXOut + hexYOut;
-  }
 
-  getSystemInformation (name) {
-      let response = "";
-      //const urlTravellerMapSearch = "https://travellermap.com/api/search?q=";
-      //urlTravellerMapSearch += name;
-     // const urlResponse = fetch(urlTravellerMapSearch);
-     // const systemDetails = urlResponse.json(); //extract JSON from the http response
-     const systemDetails = {"Results":{"Count":1,"Items":[{"World":{"HexX":29,"HexY":6,"Sector":"Deneb","Uwp":"C5A5422-A","SectorX":-3,"SectorY":-1,"Name":"Nurara","SectorTags":"Official OTU"}}]}};
-
-      
-      // do something with system details
-      const worldDetails = systemDetails.Results.Items[0].World;
-      response += `The ${this.toTitleCase(name)} system is in the ${worldDetails.Sector} sector\n`;
-      response += `It has a UWP of ${worldDetails.Uwp}\n`;
-      const hexCode =this.makeHexCode(worldDetails.HexX, worldDetails.HexY)
-      response += `The Hex Code for this system is ${hexCode}\n`;
-
-      const moreDetails = {"Worlds":[{"Name":"Nurara","Hex":"2906","UWP":"C5A5422-A","PBG":"604","Zone":"","Bases":"S","Allegiance":"ImDd","Stellar":"M0 V","SS":"D","Ix":"{ 0 }","CalculatedImportance":0,"Ex":"(A33-4)","Cx":"[1416]","Nobility":"B","Worlds":9,"ResourceUnits":-360,"Subsector":3,"Quadrant":1,"WorldX":-68,"WorldY":-74,"Remarks":"Fl Ni","LegacyBaseCode":"S","Sector":"Deneb","SubsectorName":"Million","SectorAbbreviation":"Dene","AllegianceName":"Third Imperium, Domain of Deneb"}]};
-      const world = moreDetails.Worlds[0];
-      if (world.Zone == "A") {
-          response += "This world is an **Amber** zone\n";
-      }else{
-          if (world.Zone == "R") {
-            response += `This world is a **RED** zone\n`;
-        }else {
-            response += "This world in **not** an Amber or Red Zone world\n";
+    makeHexCode(hexX, hexY) {
+        let hexXOut = hexX.toString();
+        let hexYOut = hexY.toString();
+        if (hexXOut.length < 2) {
+            hexXOut = "0" + hexXOut;
         }
-      }
-      if( world.PBG.slice(world.PBG.length - 1) == "0") {
-        response += "There is **no** Gas Giant suitable for refueling in this system\n";
-      }else{
-          response += "There is at least one **Gas Giant** suitable for refueling in this system\n";
-      }
-      response += `It has the followng bases: ${baseCodes[world.Bases].Allegience} ${baseCodes[world.Bases].Type}\n`;
-      response += `It has the trade codes of **${world.Remarks}**\n`;
-      return response;
-  }
-
-  toTitleCase(str) {
-    return str.replace(
-        /\w\S*/g,
-        function(txt) {
-            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        if (hexYOut.length < 2) {
+            hexYOut = "0" + hexYOut;
         }
-    );
+        return hexXOut + hexYOut;
     }
 
-  async do() {
-    await super.do();
-    const commandText = this.msg.content.substring(this.prefix.length).trim();
-    const tokens = commandText.toUpperCase().split(' ');
-  
-    let response = '\n';
-    for (const name of tokens) {
-        response += `System Information: **${this.toTitleCase(name)}**\n`;
-        response += this.getSystemInformation(name);
+    parseRemarks (stringRemarks) {
+        const remarksRegex = /(.*)[\(\[](.*)/g;
+        let rem = remarksRegex.exec(stringRemarks);
+        // first item is the string searched
+        rem.shift();
+        let tcList = [];
+        let sopList = [];
+        let sophString = "";
+        if (rem) {
+            tcList = rem[0].split(" ");
+            sophString = rem[1];
+        }
+        // remove last item in tc list - it is empty
+        tcList.pop();
+        //discard the list of sophonts for now
+        //TODO parse sophonts into population splits
+        return tcList; 
     }
-    await this.msg.reply(response);
-  }
+
+    getSystemInformation(name) {
+        let response = "";
+        const urlTravellerMap = "https://travellermap.com/";
+        const urlTravellerMapSearch = urlTravellerMap + `api/search?q=${name}`;
+
+        // Call the travellermap api using name in the search api
+
+        // extract the json response
+        const systemDetails = { "Results": { "Count": 1, "Items": [{ "World": { "HexX": 27, "HexY": 4, "Sector": "Deneb", "Uwp": "C543976-A", "SectorX": -3, "SectorY": -1, "Name": "Talon", "SectorTags": "Official OTU" } }] } };
+        const worldDetails = systemDetails.Results.Items[0].World;
+
+        // make the hexcdoe to use inthe data call
+        const hexCode = this.makeHexCode(worldDetails.HexX, worldDetails.HexY)
+        // call the data api using the hexcode
+        const urlTravellerMapData = urlTravellerMap + `data/${worldDetails.Sector}/${hexCode}`
+
+        const moreDetails = { "Worlds": [{ "Name": "Talon", "Hex": "2704", "UWP": "C543976-A", "PBG": "224", "Zone": "", "Bases": "CK", "Allegiance": "VAug", "Stellar": "K4 V", "SS": "D", "Ix": "{ 4 }", "CalculatedImportance": 4, "Ex": "(H8E+3)", "Cx": "[8D49]", "Nobility": "", "Worlds": 18, "ResourceUnits": 5712, "Subsector": 3, "Quadrant": 1, "WorldX": -70, "WorldY": -76, "Remarks": "Hi In Po (Souggvuez)8 Varg2", "LegacyBaseCode": "H", "Sector": "Deneb", "SubsectorName": "Million", "SectorAbbreviation": "Dene", "AllegianceName": "United Followers of Augurgh" }] };
+        const world = moreDetails.Worlds[0];
+
+        // do something with system details
+        response += `The search url was <${urlTravellerMapSearch}>\n`;
+        response += `The data url was <${urlTravellerMapData}>\n`
+
+        response += `The ${world.Name} system is in the ${worldDetails.Sector} sector\n`;
+        response += `It has a UWP of ${worldDetails.Uwp}\n`;
+        response += `The Hex Code for this system is ${hexCode}\n`;
+
+        if (world.Zone == "A") {
+            response += "This world is an **Amber** zone\n";
+        } else {
+            if (world.Zone == "R") {
+                response += `This world is a **RED** zone\n`;
+            } else {
+                response += "This world in **not** an Amber or Red Zone world\n";
+            }
+        }
+        if (world.PBG.slice(world.PBG.length - 1) == "0") {
+            response += "There is **no** Gas Giant suitable for refueling in this system\n";
+        } else {
+            response += "There is at least one **Gas Giant** suitable for refueling in this system\n";
+        }
+        response += `This world owes allegiance to the ${world.AllegianceName}\n`;
+        response += `It has the following bases:\n`;
+        for (const b of world.Bases) {
+            response += `\t${baseCodes[b].type}\n`;
+        }
+        //parse remarks - this field is overloaded with items and needs to be unpacked. No I'm not bitter, why do you ask?
+        const tradeCodes = this.parseRemarks(world.Remarks);
+        response += `It has the trade codes of **${tradeCodes}**\n`;
+        return response;
+    }
+
+    toTitleCase(str) {
+        return str.replace(
+            /\w\S*/g,
+            function (txt) {
+                return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+            }
+        );
+    }
+
+    async do() {
+        await super.do();
+        const commandText = this.msg.content.substring(this.prefix.length).trim();
+        const tokens = commandText.toUpperCase().split(' ');
+
+        let response = '\n';
+        for (const name of tokens) {
+            response += `System Information: **${this.toTitleCase(name)}**\n`;
+            response += this.getSystemInformation(name);
+        }
+        await this.msg.reply(response);
+    }
 
 }
 
