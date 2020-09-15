c229d61747e1e0c806b217ea7ba8e0797b43a1e2
swdevlin <swdevlin@gmail.com>
2020-09-14 22:54:17 -0400

    System lookup

--- commands/system.js
+++ commands/system.js
@@ -1,59 +1,34 @@
 const BaseCommand = require("./base");
-
-const {tradeCodes, purchase, sell} = require("../trade_codes");
+const axios = require('axios').default;
 
 class System extends BaseCommand {
   constructor(prefix, msg) {
     super(prefix, msg);
-    this.codes = [];
-    this.goodName = "";
+    this.systemName = null;
   }
 
   parseMsg() {
-    let tokens = this.commandText.split(' ');
-    // get rid of the trade
-    tokens.shift();
-    this.codes = tokens.map(a => a.toUpperCase());
+    this.systemName = this.commandText;
   }
 
   makeHexCode(hexX, hexY) {
-    let hexXOut = hexX.toString();
-    let hexYOut = hexY.toString();
-    if (hexXOut.length < 2) {
-        hexXOut = "0" + hexXOut;    
-    }
-    if (hexYOut.length < 2) {
-        hexYOut = "0" + hexYOut;    
-    }
-    return hexXOut + hexYOut;
+    const x = ('0' + hexX).slice(-2);
+    const y = ('0' + hexY).slice(-2);
+    return x + y;
   }
 
-  getSystemInformation (name) {
-      let response = "";
-      //const urlTravellerMapSearch = "https://travellermap.com/api/search?q=";
-      //urlTravellerMapSearch += name;
-     // const urlResponse = fetch(urlTravellerMapSearch);
-     // const systemDetails = urlResponse.json(); //extract JSON from the http response
-     const systemDetails = {"Results":{"Count":1,"Items":[{"World":{"HexX":29,"HexY":6,"Sector":"Deneb","Uwp":"C5A5422-A","SectorX":-3,"SectorY":-1,"Name":"Nurara","SectorTags":"Official OTU"}}]}};
-
-      
-      // do something with system details
-      const worldDetails = systemDetails.Results.Items[0].World;
-      response += `The ${this.toTitleCase(name)} system is in the ${worldDetails.Sector} sector\n`;
-      response += `It has a UWP of ${worldDetails.Uwp}\n`;
-      const hexCode =this.makeHexCode(worldDetails.HexX, worldDetails.HexY)
-      response += `The Hex Code for this system is ${hexCode}\n`;
-
-      const moreDetails = {"Worlds":[{"Name":"Nurara","Hex":"2906","UWP":"C5A5422-A","PBG":"604","Zone":"","Bases":"S","Allegiance":"ImDd","Stellar":"M0 V","SS":"D","Ix":"{ 0 }","CalculatedImportance":0,"Ex":"(A33-4)","Cx":"[1416]","Nobility":"B","Worlds":9,"ResourceUnits":-360,"Subsector":3,"Quadrant":1,"WorldX":-68,"WorldY":-74,"Remarks":"Fl Ni","LegacyBaseCode":"S","Sector":"Deneb","SubsectorName":"Million","SectorAbbreviation":"Dene","AllegianceName":"Third Imperium, Domain of Deneb"}]};
-      const world = moreDetails.Worlds[0];
-      if (world.Zone == "") {
-          response += "This world in not an Amber or Red Zone world\n";
-      }else{
-          response += `This world has a zone of ${world.Zone}\n`;
+  async findSystemByName(name) {
+      const travellerMapSearchURL = `https://travellermap.com/api/search?q=${this.systemName}`;
+      const response = await axios.get(travellerMapSearchURL);
+      if (response.status !== 200) {
+        throw 'error fetching data';
       }
-      response += `It has the followng bases: ${world.Bases}\n`;
-      response += `It has the trade codes of ${world.Remarks}\n`;
-      return response;
+
+      if (response.data.Count === 0) {
+        throw 'no matches';
+      } else {
+        return response.data.Results.Items;
+      }
   }
 
   toTitleCase(str) {
@@ -63,19 +38,52 @@ class System extends BaseCommand {
             return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
         }
     );
+  }
+
+  async renderMultipleMatches(systems) {
+    let response = 'Multiple matches\n';
+    for (const system of systems) {
+      const hex = this.makeHexCode(system.World.HexX, system.World.HexY);
+      response += `${system.World.Name}, ${system.World.Sector} ${hex}\n`;
     }
+    await this.msg.reply(response);
+  }
+
+  async renderSystemData(system) {
+    const sector = system.World.Sector;
+    const hex = this.makeHexCode(system.World.HexX, system.World.HexY);
+    const travellerMapWorldURL = `https://travellermap.com/data/${sector}/${hex}`;
+    const worldResponse = await axios.get(travellerMapWorldURL);
+    if (worldResponse.status !== 200) {
+      throw 'error fetching world data';
+    }
+    const world = worldResponse.data.Worlds[0];
+    let response = `${world.Name}\n`;
+    response += `${world.UWP}\n`;
+    response += `${world.Stellar}\n`;
+    response += `${world.Remarks}\n`;
+    response += `${world.Bases}\n`;
+    response += `${world.AllegianceName}\n`;
+    await this.msg.reply(response);
+  }
 
   async do() {
     await super.do();
-    const commandText = this.msg.content.substring(this.prefix.length).trim();
-    const tokens = commandText.toUpperCase().split(' ');
-  
-    let response = '\n';
-    for (const name of tokens) {
-        response += `System Information: **${this.toTitleCase(name)}**\n`;
-        response += this.getSystemInformation(name);
+    try {
+      const systems = await this.findSystemByName();
+      if (systems.length === 1)
+        await this.renderSystemData(systems[0]);
+      else
+        await this.renderMultipleMatches(systems);
+    } catch (error) {
+      this.msg.reply('Error looking up system');
     }
-    await this.msg.reply(response);
+    // let response = '\n';
+    // for (const name of tokens) {
+    //     response += `System Information: **${this.toTitleCase(name)}**\n`;
+    //     response += this.getSystemInformation(name);
+    // }
+    // await this.msg.reply(response);
   }
 
 }
